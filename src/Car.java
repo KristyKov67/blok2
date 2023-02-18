037ae628c0903ea8e2669a1be8471f053be8e2e3
Kristy <krisstyle@inbox.ru>
2023-02-19 00:05:35 +0300

    1 задание

--- src/Car.java
+++ src/Car.java
@@ -1,82 +1,118 @@
 import java.util.Objects;
 
 public class Car {
- String brand;
- String model;
-
- @Override
- public boolean equals(Object o) {
-  if (this == o) return true;
-  if (o == null || getClass() != o.getClass()) return false;
-  Car car = (Car) o;
-  return Double.compare(car.engineVolume, engineVolume) == 0 && year == car.year && Objects.equals(brand, car.brand) && Objects.equals(model, car.model) && Objects.equals(color, car.color) && Objects.equals(country, car.country);
- }
-
- @Override
- public int hashCode() {
-  return Objects.hash(brand, model, engineVolume, color, year, country);
- }
-
- double engineVolume;
- String color;
- int year;
- String country;
-
- @Override
- public String toString() {
-  return "Car{" +
-          "brand='" + brand + '\'' +
-          ", model='" + model + '\'' +
-          ", engineVolume=" + engineVolume +
-          ", color='" + color + '\'' +
-          ", year=" + year +
-          ", country='" + country + '\'' +
-          '}';
- }
-
- public String getBrand() {
-  return brand;
- }
-
- public void setBrand(String brand) {
-  this.brand = brand;
- }
-
- public String getModel() {
-  return model;
- }
-
- public void setModel(String model) {
-  this.model = model;
- }
+    String brand;
+    String model;
 
 
- public void setEngineVolume(int engineVolume) {
-  this.engineVolume = engineVolume;
- }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return Double.compare(car.engineVolume, engineVolume) == 0 && year == car.year && Objects.equals(brand, car.brand) && Objects.equals(model, car.model) && Objects.equals(color, car.color) && Objects.equals(country, car.country);
+    }
 
- public String getColor() {
-  return color;
- }
+    @Override
+    public int hashCode() {
+        return Objects.hash(brand, model, engineVolume, color, year, country);
+    }
 
- public void setColor(String color) {
-  this.color = color;
- }
+    double engineVolume;
+    String color;
+    int year;
+    String country;
 
- public int getYear() {
-  return year;
- }
 
- public void setYear(int year) {
-  this.year = year;
- }
+    private static final String Default_Value = "default";
+    private static final double Default_engine = 1.5;
+    private static final String Default_color = "елый";
+    private static final int Default_year = 2000;
 
- public String getCountry() {
-  return country;
- }
+    public Car(String brand, String model, double engineVolume, String color, int year, String country) {
+        setBrand(brand);
+        setModel(model);
+        setEngineVolume((int) engineVolume);
+        setColor(color);
+        setYear(year);
+        setCountry(country);
+    }
 
- public void setCountry(String country) {
-  this.country = country;
- }
+    @Override
+    public String toString() {
+        return "машина: " + "бренд= " + brand + " ,марка= " + model + ", мощность =" + engineVolume +
+                " цвет='" + color + ", год=" + year +
+                ", страна='" + country + '}';
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        if (brand == null || brand.isBlank()) {
+            this.brand = Default_Value;
+        } else {
+            this.brand = brand;
+        }
+    }
+
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        if (model == null || model.isBlank()) {
+            this.model = Default_Value;
+        } else {
+            this.model = model;
+        }
+    }
+
+
+    public void setEngineVolume(int engineVolume) {
+        if (engineVolume <= 0) {
+            this.engineVolume = Default_engine;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        if (color == null || color.isBlank()) {
+            this.color = Default_color;
+        } else {
+            this.color = color;
+        }
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        if (year <= 0) {
+            this.year = Default_year;
+        } else {
+            this.year = year;
+        }
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        if (country == null || country.isBlank()) {
+            this.country = Default_Value;
+        } else {
+            this.country = country;
+        }
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,38 +2,11 @@ import org.w3c.dom.ls.LSOutput;
 
 public class Main {
     public static void main(String[] args) {
-        Car car1 = new Car();
-
-        car1.brand = "lada";
-        car1.model = "granta";
-        car1.country = "Россия";
-        car1.color = "желтый";
-        car1.engineVolume = 1.7;
-
-        Car car2 = new Car();
-        car2.brand = "audi";
-        car2.model = "a8 50 l tdi quattro";
-        car2.country = "Германия";
-        car2.color = "черный";
-        car2.engineVolume = 3;
-        car2.year = 2021;
-
-        Car car3 = new Car();
-        car3.brand = "kia";
-        car3.model = "sportage 4 поколения";
-        car3.country = "Южная Корея";
-        car3.color = "красный";
-        car3.engineVolume = 2.4;
-        car3.year = 2018;
-
-        Car car4 = new Car();
-        car4.brand = "hyndai";
-        car4.model = "avante";
-        car4.country = "Южная Корея";
-        car4.color = "оранжевый";
-        car4.engineVolume = 1.6;
-        car4.year = 2016;
-        System.out.print(car1 + " " + car2);
+        Car car1 = new Car("lada","granta",1.7,"желтый",0,"Россия" );
+        Car car2 = new Car("audi", "a8 50 l tdi quattro", 3,"черный",2021, "Германия");
+        Car car3 = new Car("kia","sportage 4 поколения",2.4, "красный", 2018,"Южная Корея");
+        Car car4 = new Car("hyndai", "avante", 1.6, "оранжевый", 2016,"Южная Корея" );
+        System.out.print(car1 + " " + car2 + ' ' + car3 + ' ' + car4 + ' ' );
 
     }
 
